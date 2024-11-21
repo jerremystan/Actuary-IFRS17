5d8aca45e7a61877db00f16249b46ace90705f5f
jerremystan <143991971+jerremystan@users.noreply.github.com>
2024-11-21 15:37:36 +0700

    Update and rename CP_SummaryExpense.v1.R to CorpPlanning_Summary_Expense.v.1.R

--- Expense/CorpPlanning_Summary_Expense.v.1.R
+++ Expense/CorpPlanning_Summary_Expense.v.1.R
@@ -5,7 +5,7 @@ library(lubridate)
 library(stringr)
 library(readxl)
 
-book <- c("TMI", "Parolamas")
+book <- c("T", "P")
 month <- c("2023-01","2023-02","2023-03","2023-04","2023-05","2023-06","2023-07","2023-08","2023-09","2023-10","2023-11","2023-12")
 group <- c("Corporate","Direct","Direct support")
 categorycode <- c("J","L")
